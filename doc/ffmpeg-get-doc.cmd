83bc0b9bf2cd4a418ae52746906bd81244927c8a
Thierry Lelegard <thierry@lelegard.fr>
2013-11-02 16:48:14 +0100

    Integrated FFmpeg 2.1

---
+++ doc/ffmpeg-get-doc.cmd
@@ -0,0 +1,7 @@
+ffmpeg -h >ffmpeg-help.txt
+ffmpeg -h full >ffmpeg-help-full.txt
+ffmpeg -formats >ffmpeg-formats.txt
+ffmpeg -codecs >ffmpeg-codecs.txt
+ffmpeg -bsfs >ffmpeg-bitstream-filters.txt
+ffmpeg -layouts >ffmpeg-layouts.txt
+ffmpeg -filters >ffmpeg-filters.txt
