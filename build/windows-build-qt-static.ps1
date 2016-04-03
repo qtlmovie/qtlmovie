87dcf052930fdbad3a60c628055286a444828055
Thierry Lelegard <thierry@lelegard.fr>
2016-04-03 13:32:02 +0200

    Use Qt 5.6.0 and FFmpeg 3.0.1

--- build/windows-build-qt-static.ps1
+++ build/windows-build-qt-static.ps1
@@ -75,7 +75,7 @@
 
 [CmdletBinding()]
 param(
-    $QtSrcUrl = "http://download.qt.io/official_releases/qt/5.5/5.5.1/single/qt-everywhere-opensource-src-5.5.1.7z",
+    $QtSrcUrl = "http://download.qt.io/official_releases/qt/5.6/5.6.0/single/qt-everywhere-opensource-src-5.6.0.7z",
     $QtStaticDir = "C:\Qt\Static",
     $QtVersion = "",
     $MingwDir = "",
--- src/QtlMovie/QtlMovieVersion.h
+++ src/QtlMovie/QtlMovieVersion.h
@@ -38,6 +38,6 @@
 //!
 //! Version of the product.
 //!
-#define QTLMOVIE_VERSION "1.5.1"
+#define QTLMOVIE_VERSION "1.5.2"
 
 #endif // QTLMOVIEVERSION_H
